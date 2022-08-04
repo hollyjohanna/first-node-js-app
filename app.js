00d654785854124b0f9f49c7741e552307d23a02
hollyjohanna <hollyjohanna.robbins@gmail.com>
2022-08-05 09:17:13 +1200

    second push

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const port = 3000;
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello World</h1><h2>this is awful</h2>");
+  console.log("another push for practise");
 });
 
 app.listen(port, () => {
